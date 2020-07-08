00c7ad4fc4d97f1c8bc83e099169bcea6a7af120
Gustavo Moreira <gustavomoreira88@gmail.com>
2020-07-08 16:22:23 -0500

    Interface finalizada

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="repositorio">
+          <img src="avatar" alt="avatar" />
+          <div>
+            <strong>repositorio</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="repositorio">
+          <img src="avatar" alt="avatar" />
+          <div>
+            <strong>repositorio</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="repositorio">
+          <img src="avatar" alt="avatar" />
+          <div>
+            <strong>repositorio</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
